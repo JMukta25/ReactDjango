da823c39e92eeab8b991a156d01d4b28fc879ba8
Abhir1902 <73550795+Abhir1902@users.noreply.github.com>
2022-12-29 15:54:15 +0530

    Changed Server

--- Server/DjangoServer/api/serializers.py
+++ Server/DjangoServer/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from djangoRestApp.models import User 
-
+from djangoRestApp.models import ticket 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User 
         fields = '__all__'
+
+class TicketSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = ticket 
+        fields = '__all__' 
